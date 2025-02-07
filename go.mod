70fbaf2589b4c63cc1068de96b3d5a1bb7374d58
PracticallyWrong <practicallywrong@proton.me>
2025-02-07 18:56:17 +0530

    hehe

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module lol
 
 go 1.23.6
+
+require github.com/mssola/useragent v1.0.0
--- main.go
+++ main.go
@@ -3,14 +3,53 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/mssola/useragent"
 )
+
+func logVisitorInfo(r *http.Request) {
+	ip := r.Header.Get("CF-Connecting-IP") // Real IP from Cloudflare
+	if ip == "" {
+		ip = r.RemoteAddr // Fallback if not behind Cloudflare
+	}
+
+	ua := useragent.New(r.UserAgent())
+	browser, browserVersion := ua.Browser()
+	os := ua.OS()
+	device := "Desktop"
+	if ua.Mobile() {
+		device = "Mobile"
+	}
+
+	referer := r.Referer()
+	cfCountry := r.Header.Get("CF-IPCountry")
+	cfCity := r.Header.Get("CF-IPCity")
+
+	log.Printf(`Visitor Info:
+	IP: %s
+	User-Agent: %s
+	Browser: %s %s
+	OS: %s
+	Device: %s
+	Referer: %s
+	Country: %s
+	City: %s`,
+		ip, r.UserAgent(), browser, browserVersion, os, device,
+		referer, cfCountry, cfCity)
+}
+
+func handlerWithLogging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logVisitorInfo(r)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	staticDir := "./web"
-
 	fs := http.FileServer(http.Dir(staticDir))
 
-	http.Handle("/", fs)
+	http.Handle("/", handlerWithLogging(fs))
 
 	port := "0.0.0.0:80"
 	log.Printf("Serving static files from %s on %s", staticDir, port)
